631f32fcb8e520f9e8089855d28747bd1ffe1fcf
Robert Gebhardt <robert.gebhardt@comprezipp.com>
2021-11-22 10:43:29 -0300

    type node & tsconfig & process exit on IPC signal

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-(async () => {
-    try {
-        await main();
-    } catch (e) {
+
+process.on('SIGINT', () => process.exit()); // Receive SO IPC signal to exit process when Ctr + C
+
+main()
+    .catch((e) => {
         console.error(e);
-        throw e;
-    }
-})();
-
-
+        process.exit(1); // Exit process with error when catch Exception
+    })
+    .finally(()=>process.exit(0)); // Exit process with success
+    
+/*
+*   Main async function 
+*/
 async function main() {
-    // throw new Error("Pane no sistema");
-    return new Promise(() => true);
+    throw new Error("Pane no sistema");
 }
